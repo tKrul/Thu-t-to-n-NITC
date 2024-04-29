a5866c65cc72ed7fbe879a6b61cad6e83deddf32
tKrul <166716326+tKrul@users.noreply.github.com>
2024-04-29 23:50:33 +0700

    Create 5_4.cpp

---
+++ 5_4.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int m,n,a[100][100],b=0;
+string path="";
+void findpath(int i,int j,string path){
+    if(i==m-1&&j==n-1){
+        cout<<path<<endl;
+        b=1;
+        return;
+    }
+    if(j+1<n&&a[i][j+1]==1&&b!=1){
+        findpath(i,j+1,path+"R");
+    }
+    if(i+1<m&&a[i+1][j]==1&&b!=1){
+        findpath(i+1,j,path+"D");
+    }
+    return;
+}
+int main() {
+    cin>>m>>n;
+    for(int i=0;i<m;i++)
+        for(int j=0;j<n;j++)
+            cin>>a[i][j];
+
+    findpath(0,0,path);
+    return 0;
+}
