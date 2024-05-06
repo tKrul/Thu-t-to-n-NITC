c5a4c3e07615476db560a702c395e49ca8c74fdb
tKrul <166716326+tKrul@users.noreply.github.com>
2024-05-06 21:54:12 +0700

    Update 6_1.cpp

--- 6_1.cpp
+++ 6_1.cpp
@@ -1,20 +1,43 @@
-#include <bits/stdc++.h>
-#define endl "\n"
-using namespace std;
+#include<bits/stdc++.h>
+using namespace std;    
+bool daungoac(string s){
+    stack<char> st;
+    for(int i=0;i<s.length();i++){
+        if(s[i]=='('||s[i]=='['||s[i]=='{'){
+            st.push(s[i]);
+        }
+        if(s[i]==')'||s[i]==']'||s[i]=='}'){
+            if(st.empty()){
+                return false;
+            }
+            if(s[i]==')'&&st.top()!='('){
+                return false;
+            }
+            if(s[i]==']'&&st.top()!='['){
+                return false;
+            }
+            if(s[i]=='}'&&st.top()!='{'){
+                return false;
+            }
+            st.pop();
+        }
+    }
+    if(st.empty()){
+        return true;
+    }
+    return false;
+}
 int main(){
-	int n,m;
-	cin>>n>>m;
-	int a[n],b[n];
-	vector <vector <int> > l(n,vector <int> (m+1,0));
-	for(int i=0;i<n;i++) cin>>a[i]>>b[i];
-	for(int i=0;i<n;i++)
-	for(int j=1;j<=m;j++){
-		if(i>0){
-			if(a[i]<=j) l[i][j]=max(l[i-1][j],l[i-1][j-a[i]]+b[i]);
-			else l[i][j]=l[i-1][j];
-		}
-		else if (a[i]<=j) l[i][j]=b[i];
-	}
-	cout<<l[n-1][m];
-  return 0;
+    string s;
+    while(getline(cin, s)){
+        if(s==".")break;
+        if(daungoac(s)){
+            cout<<"yes";
+        }
+        else{
+            cout<<"no";
+        }
+        cout<<endl;
+    }
+    return 0;
 }
