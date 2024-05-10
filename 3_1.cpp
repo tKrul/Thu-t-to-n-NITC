1249ec4f323f885e9bcea6888e0fbaaadb090b9e
tKrul <166716326+tKrul@users.noreply.github.com>
2024-05-11 00:26:22 +0700

    Create 3_1.cpp

--- 3_1.cpp
+++ 3_1.cpp
@@ -4,7 +4,7 @@ int dem(string s){
     int a[2]={2,-2},b[2]={1,-1},d=0,x=s[0]-96,y=s[1]-48;
     for(int i=0;i<2;i++){
         for(int j=0;j<2;j++){
-            if((x+a[i]<=8&&x+a[i]>=1)&&(y+b[j]>=1&&y+b[j]<=8))d++;
+            if((x+a[i]<=8&&x+a[i]>=1)&&(y+b[j]>=1&&y+b[j]<=8))d++; 
         }
     }
     for(int i=0;i<2;i++){
