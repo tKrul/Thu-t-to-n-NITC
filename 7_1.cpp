be9ea0b708d2bcd40ad5afd3d27c0c81e53952d9
tKrul <166716326+tKrul@users.noreply.github.com>
2024-05-10 21:54:42 +0700

    Create 7_1.cpp

---
+++ 7_1.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,m;
+    cin>>n>>m;
+    int a[n+1][n+1]={};
+    for(int i=1;i<=m;i++){
+        int x,y;
+        cin>>x>>y;
+        a[x][y]++;
+        a[y][x]++;
+    }
+    for(int i=1;i<=m;i++){
+        for(int j=1;j<=m;j++){
+            cout<<a[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
