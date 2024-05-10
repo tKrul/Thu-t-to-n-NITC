b9b59a57821fd9e8fa567b85b8d1b954d83cd5b3
tKrul <166716326+tKrul@users.noreply.github.com>
2024-05-11 00:28:59 +0700

    Create 2_1.cpp

--- 2_1.cpp
+++ 2_1.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 bool checkpalin[103][103];
 void check(string s){
-    int n=s.length();
+    int n=s.length(); 
     for(int i=n-1; i>=0; i--){
         checkpalin[i][i] =1;
         for(int j=i+1;j<n;j++) {
