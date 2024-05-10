c9716abedf73ebe991533f69984a74176ae184c8
tKrul <166716326+tKrul@users.noreply.github.com>
2024-05-11 00:29:15 +0700

    Create 2_2.cpp

--- 2_2.cpp
+++ 2_2.cpp
@@ -3,7 +3,7 @@ using namespace std;
 string rut(string &s){
     while(s!=""){
         if((int)(s[0]-'0')%3==0) {
-            s.erase(0,1);
+            s.erase(0,1); 
         }
         else if((int)(s[s.size()-1]-'0')%3==0) {
             s.erase(s.size()-1,1);
